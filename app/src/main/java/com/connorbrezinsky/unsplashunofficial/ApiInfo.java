6dfb6ee47aae793ca8b8c6fe3865e681dd68971c
rye761 <ryebread761@gmail.com>
2016-07-12 15:03:14 -0600

    Demo items in list

---
+++ app/src/main/java/com/connorbrezinsky/unsplashunofficial/ApiInfo.java
@@ -0,0 +1,10 @@
+package com.connorbrezinsky.unsplashunofficial;
+
+/**
+ * Created by ryan on 6/30/16.
+ */
+public class ApiInfo {
+    public static final String API_APPID = "ec33bbfe672d64f8f8ff0ca32b47c5ebb95d89c1ae073ac607d483dd9d497173";
+    public static final String API_SECRET = "9ee44a7902aa2b7d9d100a8d82b870ef88ee1199aca0c7bbeeab46f39c080e77";
+    public static final String API_CALLBACK = "urn:ietf:wg:oauth:2.0:oob";
+}
